7da1819f74bff8e7d9a6256500914f2a19c21a31
IvElGrimm <126293620+Ivaname99@users.noreply.github.com>
2024-09-05 21:31:21 -0600

    Creacion de nuevas clases

--- linqGuia/Program.cs
+++ linqGuia/Program.cs
@@ -131,3 +131,41 @@ if (h1 == null)
 }
 Console.WriteLine(h1.datosHabitante());
 #endregion
+
+#region ElementAt
+var terceraCasa = listaCasa.ElementAt(2);
+Console.WriteLine($"La tercera casa es {terceraCasa.dameDatosCasa()}");
+
+var casaError = listaCasa.ElementAtOrDefault(3);
+if (casaError != null) { Console.WriteLine($"La tercera casa es {casaError.dameDatosCasa()}"); }
+
+var segundoHabitante = (from objetoTem in listaHabitante select objetoTem).ElementAtOrDefault(2);
+Console.WriteLine($" segundo habitante es : {segundoHabitante.datosHabitante()}");
+
+#endregion
+
+#region single
+try
+{
+    var habitantes = listaHabitante.Single(variableTem => variableTem.Edad > 40 && variableTem.Edad < 70);
+    // Creando esta consulta pero con LinQ
+    var habitante2 = (from obtem in listaHabitante where obtem.Edad > 70 select obtem).SingleOrDefault();
+
+    Console.WriteLine($"habitante con menos de 20 años {habitantes.datosHabitante()}");
+    if (habitante2 != null) Console.WriteLine($"habitante con mas de 70 años {habitante2.datosHabitante()}");
+}
+catch (Exception)
+{
+    Console.WriteLine($"Ocurrio el error");
+}
+#endregion
+
+#region typeOf
+var listaEmpleados = new List<Empleado>() {
+    new Medico(){ nombre= "Jorge Casa" },
+    new Enfermero(){ nombre = "Raul Blanco"}
+};
+
+var medico = listaEmpleados.OfType<Medico>();
+Console.WriteLine(medico.Single().nombre);
+#endregion
